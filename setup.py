c83738a72a0b67c6ec12a6d1671fd0913641f83b
Alfred Haugen <waa4bq@virginia.edu>
2023-10-27 21:53:04 -0400

    edited

--- setup.py
+++ setup.py
@@ -1,20 +1,14 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-    name = 'Mod_9_Project',
-    version = '0.1.0',
-    author = 'The Best!',
-    author_email = 'aac@example.com',
-    packages = ['Demo'],
-    scripts = [],
-    url = ',
-    license = 'LICENSE.txt',
-    description = 'An awesome package that does something',
-    long_description = open('README.txt').read(),
-    install_requires = [
-        "Django >= 1.1.1",
-        "pytest",
-    ],
+    name='Mod_9_Project',
+    version='1.0.0',
+    url='https://github.com/alfskoyen/Mod_9_Project',
+    author='Alfred Haugen',
+    author_email='author@gmail.com',
+    description='Description of my package',
+    packages=find_packages(),    
+    install_requires=['numpy >= 1.11.1', 'matplotlib >= 1.5.1'],
 )
 
 
